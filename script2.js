ac5db9701fa33b98e4464ac4ccaa47cd4f4828e6
pedrobrice91 <pedrobrice91@gmail.com>
2025-03-31 11:39:18 -0300

    ordenando

---
+++ script2.js
@@ -0,0 +1,11 @@
+
+/* Solicitar al usuario que indique dos numeros 
+y que nos diga cual es el mayor, el menor y
+ si son iguales 
+ Plus:si los numeros no son numeros o son menores
+  o iguales a cero nos vuelva a pedir 
+ */
+
+  
+  
+  
