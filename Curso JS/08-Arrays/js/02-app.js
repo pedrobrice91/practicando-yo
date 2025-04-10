e682f9d08499945beacda294cb51adaee6f515bc
pedrobrice91 <pedrobrice91@gmail.com>
2025-04-10 17:00:27 -0400

    Hoy

--- Curso JS/08-Arrays/js/02-app.js
+++ Curso JS/08-Arrays/js/02-app.js
@@ -7,7 +7,9 @@ const numeros = [10,20,30,40,50];
 console.table(numeros);
 
 
-// La forma en la que accedes a un arreglo es por lo que se econoce como el indice en el arreglo, los arreglos inician en 0 usualmente aunuque hay lenguajes donde inician en 1
+// La forma en la que accedes a un arreglo es por lo que se 
+// econoce como el indice en el arreglo, los arreglos inician
+//  en 0 usualmente aunuque hay lenguajes donde inician en 1
 
 console.log(numeros[0])
 console.log(numeros[1])
