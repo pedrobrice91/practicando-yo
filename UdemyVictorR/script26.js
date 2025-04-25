c40f3839ea172a9e60858156931db37b0be76cd6
Pedro L Briceno <pedrobrice91@gmail.com>
2025-04-25 10:29:36 -0400

    viernes

--- UdemyVictorR/script26.js
+++ UdemyVictorR/script26.js
@@ -4,15 +4,10 @@ y crea un nuevo array que contenga solo esos numeros
 
 let palabrasVarias = ["perro", "sol", "luis", "paraleletipedo"];
 
-for (palabrasVarias of palabrita){
-    console.log(palabrita)
-}
-
 let nuevoArray = [];
+let aa 
 
 palabrasVarias.forEach(Element => {
-    console.log("hola"+ Element.length)
+   aa = nuevoArray.push(Element.length)
 })
-
-
-//El método join() se utiliza para convertir un array en una cadena de texto, no para agregar elementos a un array.
+console.log(nuevoArray)
---
+++ UdemyVictorR/script28.js
@@ -0,0 +1,19 @@
+/* 
+Pide 6 numeros por pantalla y metelos en un array 
+Muestra el array entero (todos sus elementos) en el cuerpo de la pagina y en la consola 
+ordenalo y muestralo
+invierte su orden y muestralo
+Muestra cuantos elementos tiene el array 
+Busca uno de los valores que ha metido el usuario, que nos diga si lo encuentra y su indice 
+Refactoriza todo lo posible usando funciones
+*/
+let numArray = [];
+
+let num1 = parseInt(prompt("Ingresa numero del array"))
+let num2 = parseInt(prompt("Ingresa numero del array"))
+let num3 = parseInt(prompt("Ingresa numero del array"))
+let num4 = parseInt(prompt("Ingresa numero del array"))
+let num5 = parseInt(prompt("Ingresa numero del array"))
+let num6 = parseInt(prompt("Ingresa numero del array"))
+
+numArray.form(num1, num2, num3, num4, num5, num6)
